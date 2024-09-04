f61a7d0a7144628aaf202aafd1652ae0cb72d18c
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-09-04 01:30:04 -0300

    chore: :sparkles: Clase N°33: Ejemplo Usando useRef

--- src/hooks/useEventsData.ts
+++ src/hooks/useEventsData.ts
@@ -1,5 +1,5 @@
 import eventsJSON from '@data/events.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Events = typeof eventsJSON;
 
@@ -7,16 +7,37 @@ type Event = Events['_embedded']['events'][number];
 
 interface useEventDataReturn {
 	events: Event[];
+	isLoading: boolean;
+	error: Error | undefined;
 }
 
 function useEventData(): useEventDataReturn {
-	const [data] = useState<Events>(eventsJSON);
-	const {
-		_embedded: { events },
-	} = data;
+	const [data, setData] = useState<Events>();
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState<Error>();
+
+	useEffect(() => {
+		setTimeout(() => {
+			try {
+				setData(eventsJSON);
+				setIsLoading(false);
+			} catch (error) {
+				if (error instanceof Error) {
+					setError(error);
+				}
+			}
+
+			// console.log(data.current);
+		}, 2000);
+	}, []);
+	// eslint-disable-next-line no-console
+	console.log(data);
 
 	return {
-		events,
+		// eslint-disable-next-line no-underscore-dangle
+		events: data?._embedded.events ?? [],
+		isLoading,
+		error,
 	};
 }
 
